7fba13a58d8cc346b9872e9cfc2c5522a85d763b
Fabricio <fabricio10.90@hotmail.com>
2018-10-27 17:31:11 -0300

    Feito o modelo do Main (usuario e bibliotecario)

--- src/view/InterfaceBibliotecario.java
+++ src/view/InterfaceBibliotecario.java
@@ -1,7 +1,42 @@
 package view;
 
+import controller.comando.Command;
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
 public class InterfaceBibliotecario {
+
+    public static void menu() {
+        System.out.println("Administra��o da Loja");
+        System.out.println("0 - Sair");
+        System.out.println("1 - Cadastrar Livro");
+        System.out.println("2 - Alterar Livro");
+        System.out.println("3 - Excluir Livro");
+        System.out.println("4 - Listar Livros");
+        System.out.println("Escolha uma op��o:");
+
+    }
+
     public static void main(String[] args) {
-        // TODO main para bibliotecário
+
+
+        int opcao = 0;
+        Scanner entrada = new Scanner(System.in);
+
+        Hashtable<Integer, Command> comandos =
+                new Hashtable<Integer, Command>();
+        /*comandos.put(1, );
+        comandos.put(4, );
+        comandos.put(0, );*/
+
+        do {
+            menu();
+            opcao = entrada.nextInt();
+            comandos.get(opcao).execute(entrada);
+
+        } while (opcao != 0);
+
+        entrada.close();
     }
 }
--- src/view/InterfaceUsuario.java
+++ src/view/InterfaceUsuario.java
@@ -1,7 +1,42 @@
 package view;
 
+import controller.comando.Command;
+
+import java.util.Hashtable;
+import java.util.Scanner;
+
 public class InterfaceUsuario {
-    public static void main(String[] args) {
-        // TODO main para usuário
+
+        public static void menu() {
+            System.out.println("Administração da Loja");
+            System.out.println("0 - ");
+            System.out.println("1 - ");
+            System.out.println("2 - ");
+            System.out.println("3 - ");
+            System.out.println("4 - ");
+            System.out.println("Escolha uma opção:");
+
+        }
+
+        public static void main(String[] args) {
+
+
+            int opcao = 0;
+            Scanner entrada = new Scanner(System.in);
+
+            Hashtable<Integer, Command> comandos =
+                    new Hashtable<Integer, Command>();
+        /*comandos.put(1, );
+        comandos.put(4, );
+        comandos.put(0, );*/
+
+            do {
+                menu();
+                opcao = entrada.nextInt();
+                comandos.get(opcao).execute(entrada);
+
+            } while (opcao != 0);
+
+            entrada.close();
     }
 }
